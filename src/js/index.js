ed8edc2ccd7aaefa4309502c0dfb42f737ddb89a
Victor Perez <github.i9aqz@passmail.com>
2025-02-08 22:07:41 +0000

    Entrada mejorada

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,36 @@ const welcomeScreen = document.querySelector(".welcome-screen");
 const msgWelcome = document.querySelector(".welcome-actions");
 const webAudio = new Audio("/src/assets/audio/homeAudio.mp3");
 const btnAudioAlow = document.querySelector(".audioBtn");
-console.log(webAudio);
+const body = document.querySelector("body");
+body.style.overflow = "hidden";
 
-window.addEventListener("DOMContentLoaded", () => {
-    msgWelcome.classList.add("opacity-1");
-    btnAudioAlow.addEventListener("click", () => {
-        msgWelcome.classList.remove("opacity-1");
-        webAudio.play();
-        webAudio.volume = 0.1;
-        msgWelcome.addEventListener("transitionend", () => {
-            setTimeout(() => {
-                welcomeScreen.style.opacity = "0";
-                welcomeScreen.addEventListener("transitionend", () => {
-                    welcomeScreen.style.display = "none";
-                });
-            }, 3000);
-        });
+msgWelcome.classList.add("opacity-1");
+btnAudioAlow.addEventListener("click", () => {
+    msgWelcome.classList.remove("opacity-1");
+    webAudio.play();
+    webAudio.volume = 0.05;
+    msgWelcome.addEventListener("transitionend", () => {
+        setTimeout(() => {
+            welcomeScreen.style.opacity = "0";
+            welcomeScreen.addEventListener("transitionend", () => {
+                welcomeScreen.style.display = "none";
+                body.style.overflow = "";
+            });
+        }, 3000);
     });
 });
+
+// let memoryScroll = 0;
+// window.addEventListener("scroll", () => {
+//     let currentScrollTop = window.scrollY;
+
+//     if (currentScrollTop > memoryScroll) {
+//         Array.from(document.querySelectorAll(".transitioned-section")).forEach(
+//             (item) => {
+//                 let itemScrollTop = item.scrollTop;
+//                 if (currentScrollTop >= itemScrollTop + 500) {
+//                 }
+//             }
+//         );
+//     }
+// });
