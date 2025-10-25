7601f0ff6b46b4f939f250941608cd627c5e156b
Victor Perez <tortoise.code@protonmail.com>
2025-10-25 11:26:36 +0100

    refactpr(bbdd): refactorizacion de la bbdd + introduccion de imagenes de proyectos

--- src/js/utils/images.js
+++ src/js/utils/images.js
@@ -1,16 +1,11 @@
 export const getImage = (name, folders = []) => {
-    
     const folderPath = folders.length > 0 ? `${folders.join("/")}/` : "";
     return new URL(`../../assets/images/${folderPath}${name}`, import.meta.url)
         .href;
 };
 
 export const getImageSize = (itemToSplit, dimension) => {
-    if (
-        !itemToSplit ||
-        itemToSplit.length === 0
-    )
-        return undefined;
+    if (!itemToSplit || itemToSplit.length === 0) return undefined;
     const splitNumber = dimension === "width" ? 0 : 1;
     return Number(itemToSplit.split("_")[1].split("x")[splitNumber]);
 };
