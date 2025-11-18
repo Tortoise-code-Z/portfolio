c89c2145ffd1886f7c0db09f85590904f554ea64
Victor Perez <tortoise.code@protonmail.com>
2025-11-18 13:49:45 +0000

    feat(project-detail/hero)

--- src/pages/ProjectDetail/index.js
+++ src/pages/ProjectDetail/index.js
@@ -1,3 +1,25 @@
-export default function ProjectDetail(props) {
-    return `<h1>ProjectDetail: ${props.params.id} </h1>`;
+import { createDiv, createFragment } from "../../js/utils/createElementsHelper";
+import { append } from "../../js/utils/domHelpers";
+import { validateProp, warningUnknownKeys } from "../../js/utils/utils";
+import Hero from "./Hero";
+import "./index.css";
+
+export default function ProjectDetail({
+    currentPath,
+    params,
+    queries,
+    navigate,
+} = {}) {
+    warningUnknownKeys(arguments, [
+        "currentPath",
+        "params",
+        "queries",
+        "navigate",
+    ]);
+
+    const fragment = createFragment();
+
+    append(fragment, [Hero({ id: params.id })]);
+
+    return fragment;
 }
