a55233b61d31bd93886e2bda19ac5a5b677cbd40
Victor Perez <tortoise.code@protonmail.com>
2025-12-10 11:26:01 +0000

    fix: Corrección definitiva de la ruta de importación del módulo.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { HashRouter } from "@tortoise-code-z/shell-io/src/shell-io.js";
+import { HashRouter } from "@tortoise-code-z/shell-io";
 import Home from "./src/pages/Home";
 import ProjectDetail from "./src/pages/ProjectDetail";
 import DefaultLayout from "./src/Layouts/DefaultLayout";
