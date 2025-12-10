8af359a33f529752ea963c0501d75c5384e1f902
Victor Perez <tortoise.code@protonmail.com>
2025-12-10 13:02:22 +0000

    feat: Integración manual de router para evitar el error 403 Forbidden.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { HashRouter } from "@tortoise-code-z/shell-io";
+import { HashRouter } from "./vender/shellio/shell-io.es.js";
 import Home from "./src/pages/Home";
 import ProjectDetail from "./src/pages/ProjectDetail";
 import DefaultLayout from "./src/Layouts/DefaultLayout";
