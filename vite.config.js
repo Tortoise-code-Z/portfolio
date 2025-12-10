8d6dd0d5e8bb935231989e47cff7fbee0854c200
Victor Perez <tortoise.code@protonmail.com>
2025-12-10 12:09:46 +0000

    fix: Configuracion de optimizeDeps para forzar la transpilacion de shell-io.

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "/portfolio/",
+    optimizeDeps: {
+        include: ["@tortoise-code-z/shell-io"],
+    },
+
+    build: {
+        commonjsOptions: {
+            include: [/node_modules/],
+        },
+    },
 });
