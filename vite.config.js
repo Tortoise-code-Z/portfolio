0c8e8d69112ff33099052c7e05a04739df7ed510
Tortoise-code-Z <tortoise.code@protonmail.com>
2026-01-27 11:49:02 +0000

    fix: solucionando importacion de html page con vite.config.js. Prueba debido a github pages.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
+// vite.config.js
 import { defineConfig } from "vite";
+import { resolve } from "path"; // Necesitarás importar esto
 
 export default defineConfig({
   base: "/portfolio/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        // Añade aquí la ruta real a tu HTML de detalle
+        detail: resolve(__dirname, "src/pages/ProjectDetail/index.html"),
+      },
+    },
+  },
 });
