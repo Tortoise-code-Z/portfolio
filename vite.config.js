385d91f73aea197160ff05b269e19384e512953f
Tortoise-code-Z <tortoise.code@protonmail.com>
2026-01-27 12:00:32 +0000

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
+// vite.config.js
 import { defineConfig } from "vite";
+import { resolve } from "path"; // Necesitarás importar esto
 
 export default defineConfig({
   base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        detail: resolve(__dirname, "./src/pages/ProjectDetail/index.html"),
+      },
+    },
+  },
 });
