7efbda96873b2d35f6e15299eadf6ca04f0d0a52
Tortoise-code-Z <tortoise.code@protonmail.com>
2026-01-27 11:51:13 +0000

    fix: volviendo a estado anterior. Prueba fallida

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,5 @@
-// vite.config.js
 import { defineConfig } from "vite";
-import { resolve } from "path"; // Necesitarás importar esto
 
 export default defineConfig({
   base: "/portfolio/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        // Añade aquí la ruta real a tu HTML de detalle
-        detail: resolve(__dirname, "src/pages/ProjectDetail/index.html"),
-      },
-    },
-  },
 });
